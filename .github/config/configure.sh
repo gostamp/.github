4cb050cc2eba10b08da0325cefe28893d49a180c
Skip Baney <twelvelabs@gmail.com>
2022-10-21 14:13:58 -0500

    fix: check for tag protection rule before trying to set

--- .github/config/configure.sh
+++ .github/config/configure.sh
@@ -4,30 +4,43 @@ set -o errexit -o errtrace -o nounset -o pipefail
 org_name="gostamp"
 repos="$(gh repo list "${org_name}" --source --json nameWithOwner --jq '.[].nameWithOwner')"
 
-echo "Configuring the following repos:"
-echo ""
+echo "::group::Repos to configure:"
 echo "${repos}"
-echo ""
+echo "::endgroup::"
+
+# shellcheck disable=SC2086
+echo "# Configured the following repos :wrench:" >>$GITHUB_STEP_SUMMARY
+# shellcheck disable=SC2086
+echo "" >>$GITHUB_STEP_SUMMARY
 
 for repo in $repos; do
     export GH_REPO="${repo}"
 
+    # shellcheck disable=SC2086
+    echo "- ${repo}" >>$GITHUB_STEP_SUMMARY
+
     echo "[${repo}] Configuring repo"
     gh api -X PATCH /repos/:owner/:repo \
-        --input=".github/config/repo.json"
+        --input=".github/config/repo.json" >/dev/null
 
     echo "[${repo}] Configuring repo collaborators"
     gh api -X PUT /orgs/:owner/teams/maintainers/repos/:owner/:repo \
-        -f "permission=maintain"
+        -f "permission=maintain" >/dev/null
 
     echo "[${repo}] Configuring branch and tag protection rules"
     gh api -X PUT /repos/:owner/:repo/branches/main/protection \
-        --input=".github/config/branch-protection.json"
-    gh api -X POST /repos/:owner/:repo/tags/protection -f pattern='v.*'
+        --input=".github/config/branch-protection.json" >/dev/null
+    # There doesn't appear to be an idempotent way to set tag protection rules,
+    # so we have to check to see if one exists first.
+    rule_id=$(gh api /repos/:owner/:repo/tags/protection \
+        --jq '.[] | select(.pattern=="v.*") | .id')
+    if [[ "${rule_id}" == "" ]]; then
+        gh api -X POST /repos/:owner/:repo/tags/protection -f pattern='v.*' >/dev/null
+    fi
 
     echo "[${repo}] Configuring automated security features"
-    gh api -X PUT /repos/:owner/:repo/vulnerability-alerts
-    gh api -X PUT /repos/:owner/:repo/automated-security-fixes
+    gh api -X PUT /repos/:owner/:repo/vulnerability-alerts >/dev/null
+    gh api -X PUT /repos/:owner/:repo/automated-security-fixes >/dev/null
 
     # Check for rate limit
     remaining="$(gh api /rate_limit --jq '.rate.remaining')"
